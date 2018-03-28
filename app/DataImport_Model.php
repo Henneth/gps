3a0a137983799756e09152929fd94cf914bed550
Henneth <henneth@Henneths-MacBook-Pro.local>
2018-03-29 01:52:19 +0800

    first draft

--- app/DataImport_Model.php
+++ app/DataImport_Model.php
@@ -10,17 +10,16 @@ use Illuminate\Database\Eloquent\Model;
 class DataImport_Model extends Model
 {
 	public static function storeGPSData($longitude, $longitude_logo, $longitude_final, $latitude, $latitude_logo, $latitude_final, $date, $time) {
-		DB::insert("INSERT INTO gps
-            (`longitude`,`longitude_logo`, `longitude_final`,`latitude`,`latitude_logo`, `latitude_final`, `date`, `time`)
-            VALUES (:longitude, :longitude_logo, :longitude_final, :latitude, :latitude_logo, latitude_final, :date, :time)", [
+		DB::insert("INSERT INTO gps_data
+            (`longitude`,`longitude_logo`, `longitude_final`,`latitude`,`latitude_logo`, `latitude_final`, `datetime`)
+            VALUES (:longitude, :longitude_logo, :longitude_final, :latitude, :latitude_logo, latitude_final, :datetime)", [
                 "longitude"=>$longitude,
                 "longitude_logo"=>$longitude_logo,
                 "longitude_final"=>$longitude_final,
                 "latitude"=>$latitude,
                 "latitude_logo"=>$latitude_logo,
                 "latitude_final"=>$latitude_final,
-                "date"=>$date,
-                "time"=>$time
+                "datetime"=>$date.' '.$time
             ]);
 	}
 	public static function insertRawData($json) {

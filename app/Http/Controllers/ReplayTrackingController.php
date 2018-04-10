70e7085486eea096f9356628582c6e6c5b6f357d
henneth <henneth.cheng@gmail.com>
2018-04-10 11:03:17 +0800

    timezone problem

--- app/Http/Controllers/ReplayTrackingController.php
+++ app/Http/Controllers/ReplayTrackingController.php
@@ -16,9 +16,9 @@ class ReplayTrackingController extends Controller {
             $array[$value->device_id][] = $value;
         }
         $jsonData = json_encode($array);
-        $timestamp_from = strtotime($event->datetime_from." UTC");
+        $timestamp_from = strtotime($event->datetime_from." HKT");
         echo $timestamp_from.' ';
-        $timestamp_to = strtotime($event->datetime_to." UTC");
+        $timestamp_to = strtotime($event->datetime_to." HKT");
         echo $timestamp_to;
         // return view('replay-tracking')->with(array('data' => $jsonData, 'event_id' => $event_id, 'timestamp_from' => $timestamp_from, 'timestamp_to' => $timestamp_to));
     }

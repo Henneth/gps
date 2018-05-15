3655f5c7078e774c1c0e25074b744711e7bf0b77
henneth <henneth.cheng@gmail.com>
2018-05-15 11:20:23 +0800

    event type error handling

--- app/Http/Controllers/EditEventController.php
+++ app/Http/Controllers/EditEventController.php
@@ -12,6 +12,9 @@ class EditEventController extends Controller {
         return view('edit-event')->with(array('event' => $event, 'event_id' => $event_id));
     }
     public function postEditEvent($event_id) {
+        if (empty($_POST['optionsRadios'])) {
+            return redirect('edit-event')->with('error', 'Event type must not be empty.');
+        }
         $datetime_from = $_POST['start-time'];
         $datetime_to = $_POST['end-time'];
         $event_type = $_POST['optionsRadios'];

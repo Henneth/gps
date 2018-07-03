4824dca46e901b73064d2023c80ec1bba007590d
henneth <henneth.cheng@gmail.com>
2018-07-03 14:19:05 +0800

    test replay data

--- app/Http/Controllers/ReplayTrackingController.php
+++ app/Http/Controllers/ReplayTrackingController.php
@@ -59,7 +59,7 @@ class ReplayTrackingController extends Controller {
         $routeIndexesByDevice = json_encode($routeIndexesByDevice);
         $profile = DeviceMapping_Model::getAthletesProfile($event_id);
 
-        echo $data;
+        echo $jsonData;
 
         return view('replay-tracking')->with(array('data' => $jsonData, 'profile' => $profile, 'event_id' => $event_id, 'timestamp_from' => $timestamp_from, 'timestamp_to' => $timestamp_to, 'route' => $route, 'event'=>$event, 'routeIndexesByDevice' => $routeIndexesByDevice, 'checkpointData'=>$checkpointData, 'checkpointDistances'=>$checkpointDistances));
     }

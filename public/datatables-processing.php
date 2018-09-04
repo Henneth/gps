c580cd8b9ef38be52c59de292df35f6d0cd9dbda
henneth <henneth.cheng@gmail.com>
2018-09-04 16:39:45 +0800

    delay time diff, interchange position

--- public/datatables-processing.php
+++ public/datatables-processing.php
@@ -31,7 +31,7 @@ $primaryKey = 'id';
 $columns = array(
     array( 'db' => 'datetime', 'dt' => 0 ),
     array( 'db' => 'created_at', 'dt' => 1 ),
-    array( 'db' => 'TIMEDIFF(datetime, created_at)', 'dt' => 2 ),
+    array( 'db' => 'TIMEDIFF(created_at, datetime)', 'dt' => 2 ),
     array( 'db' => 'TIMEDIFF(datetime, (SELECT MAX(datetime) FROM raw_data AS r1 WHERE r1.device_id = r2.device_id AND r1.datetime < r2.datetime LIMIT 1))', 'dt' => 3 ),
     array( 'db' => 'device_id', 'dt' => 4 ),
     array( 'db' => 'longitude', 'dt' => 5 ),
